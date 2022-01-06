6c3586ed8479e97b8ae03e7f0263ac4b22c589f0
Deepti <64003317+deepti23-web@users.noreply.github.com>
2022-01-06 19:17:01 +0530

    Time: 54 ms (40.79%), Space: 18.9 MB (49.87%) - LeetHub

---
+++ 875-koko-eating-bananas/875-koko-eating-bananas.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    int res=-1;
+    bool isValid(vector<int>& arr,int h,int mid){
+       long time=0;
+        
+        for(int i=0;i<arr.size();i++){
+            
+            time+= ceil((double) arr[i]/(double) mid);
+          
+        }
+        if(time<=h)return true;
+        
+        return false;
+    }
+    int minEatingSpeed(vector<int>& piles, int h) {
+        int start=1;
+        int n= piles.size();
+        int end=*max_element(piles.begin(),piles.end());;
+        while(start<=end){
+            int mid= start + ( end-start)/2;
+            cout<<mid<<" ";
+            if(isValid(piles,h, mid)){
+                res=mid;
+                end=mid-1;
+            }
+            else
+                start=mid+1;
+        }
+        return res;
+    }
+};
