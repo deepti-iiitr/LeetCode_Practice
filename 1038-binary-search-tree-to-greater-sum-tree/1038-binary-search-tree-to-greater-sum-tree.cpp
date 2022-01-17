062ba53c7056798335297c4aaf53cdccb5f52bd5
Deepti <64003317+deepti23-web@users.noreply.github.com>
2022-01-17 19:08:13 +0530

    Time: 39 ms (6.59%), Space: 16.4 MB (6.59%) - LeetHub

---
+++ 1038-binary-search-tree-to-greater-sum-tree/1038-binary-search-tree-to-greater-sum-tree.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    vector<int> a;
+    void inorder(TreeNode* root)
+    {
+        if(root!=NULL)
+        {
+            inorder(root->left);
+            a.push_back(root->val);
+            inorder(root->right);
+        }
+    }
+
+    void createTree(unordered_map<int,int> map,TreeNode* root)
+    {
+        if(root!=NULL){
+            createTree(map,root->left);
+           createTree(map,root->right);
+           root->val = map[root->val]; 
+        }
+    }
+    TreeNode* bstToGst(TreeNode* root) {
+        inorder(root);
+        unordered_map<int,int> map;
+        int sum=0;
+        for(int i=a.size()-1;i>=0;i--)
+        {
+            sum +=a[i];
+            map[a[i]] = sum;
+        }
+        createTree(map,root);
+        return root;
+    }
+};
