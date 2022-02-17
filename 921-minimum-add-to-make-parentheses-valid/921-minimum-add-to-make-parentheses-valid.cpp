779b51a94ef3de62e0bd4a51e2eddfbc34c4c60d
Deepti <64003317+deepti23-web@users.noreply.github.com>
2022-02-17 15:58:31 +0530

    Time: 5 ms (15.45%), Space: 6.4 MB (20.73%) - LeetHub

---
+++ 921-minimum-add-to-make-parentheses-valid/921-minimum-add-to-make-parentheses-valid.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int minAddToMakeValid(string s) {
+        stack<int>st;
+        for(int i=0;i<s.length();i++){
+            if(!st.empty() && st.top()=='(' && s[i]==')')
+                st.pop();
+            else
+                st.push(s[i]);
+        }
+        return st.size();
+    }
+};
