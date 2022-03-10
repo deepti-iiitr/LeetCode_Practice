b075164eed33073a845bdc372b42261d00c877a0
Deepti <64003317+deepti23-web@users.noreply.github.com>
2022-03-10 15:29:22 +0530

    Time: 9 ms (60.48%), Space: 7.4 MB (55.02%) - LeetHub

---
+++ 51-n-queens/51-n-queens.cpp
@@ -0,0 +1,42 @@
+class Solution {
+    vector<vector<string>> ans;
+public:
+    bool isSafe(vector<string> &board, int row, int col){
+        for(int i = 0; i<board.size(); ++i){
+            if(board[i][col] == 'Q'){return false;}
+        }
+        int x = row; int y = col;
+        while(x>=0 && y>=0){
+            if(board[x][y] == 'Q'){return false;}
+            --x;  --y; 
+        }
+        while(row>=0 && col<board.size()){
+            if(board[row][col] == 'Q'){return false;}
+            --row; ++col;
+        }
+        
+        return true;
+    }
+    
+    void placeQueen(vector<string> &board, int row){
+        if(row == board.size()){
+            ans.push_back(board);
+            return;
+        }
+        
+        for(int col = 0; col<board.size() ; ++col){
+            if(isSafe(board, row, col)){
+                board[row][col] = 'Q';
+                placeQueen(board, row+1);
+                board[row][col] = '.';
+            }   
+        }
+    }
+    
+    
+    vector<vector<string>> solveNQueens(int n) { 
+            vector<string> board(n, string(n, '.'));
+            placeQueen(board, 0); 
+            return ans;
+    }
+};
