e5af9c919a7503b09b76749574357fda6124158c
Deepti <64003317+deepti23-web@users.noreply.github.com>
2022-05-11 23:33:31 +0530

    Time: 0 ms (100.00%), Space: 6 MB (26.48%) - LeetHub

---
+++ 633-sum-of-square-numbers/633-sum-of-square-numbers.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    bool judgeSquareSum(int c) {
+         long long int i=0,j=sqrt(c);
+        while(i<=j)
+        {
+            if(i*i+j*j==c)
+			{
+				return true;
+			}
+            else if(i*i+j*j>c)
+			{
+                j--;
+			}
+            else
+			{
+                i++;
+			}
+        }
+        return false;
+    }
+};
