72abbc5887a888713fa0e6811391fcf820aad4c7
Deepti <64003317+deepti23-web@users.noreply.github.com>
2022-06-13 07:28:33 +0530

    Time: 5 ms (33.05%), Space: 8.1 MB (52.30%) - LeetHub

---
+++ 553-optimal-division/553-optimal-division.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    string optimalDivision(vector<int>& nums) {
+      int n=nums.size();
+        string ans;
+        if(n==1) return ans=to_string(nums[0]);
+        if(n==2) return ans=to_string(nums[0])+"/"+to_string(nums[1]);
+        ans=to_string(nums[0]);
+        ans.append("/(");
+        for(int i=1;i<n-1;i++){
+            ans.append(to_string(nums[i])+"/");
+        }
+        ans.append(to_string(nums[n-1]));
+        ans.append(")");
+		
+        return ans;  
+    }
+};
