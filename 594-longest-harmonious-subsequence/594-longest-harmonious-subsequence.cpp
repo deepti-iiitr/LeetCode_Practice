cad070e4eb2a40685ab02461b32856c886ed0c52
Deepti <64003317+deepti-iiitr@users.noreply.github.com>
2022-08-19 17:02:47 +0530

    Time: 318 ms (5.03%), Space: 43.4 MB (15.79%) - LeetHub

--- 594-longest-harmonious-subsequence/594-longest-harmonious-subsequence.cpp
+++ 594-longest-harmonious-subsequence/594-longest-harmonious-subsequence.cpp
@@ -6,11 +6,12 @@ public:
             m[nums[i]]++;
         vector<int>v;
         for(auto i:m){
+            cout<<i.first<<" "<<i.second<<endl;
             v.push_back(i.first);
         }
-        sort(v.begin(), v.end());
+        //sort(v.begin(), v.end());
         int ans=INT_MIN;
-        int n=v.size();
+        int n=m.size();
         for(int i=0;i<n-1;i++){
             if(v[i+1]-v[i] == 1)
                 ans=max(m[v[i]] + m[v[i+1]], ans);
