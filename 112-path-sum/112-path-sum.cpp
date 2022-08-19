59e1b44529880439d911b1bc5832f60f1ba01781
Deepti <64003317+deepti-iiitr@users.noreply.github.com>
2022-08-20 01:19:31 +0530

    Time: 12 ms (81.40%), Space: 21.2 MB (92.17%) - LeetHub

--- 112-path-sum/112-path-sum.cpp
+++ 112-path-sum/112-path-sum.cpp
@@ -16,18 +16,21 @@ public:
     void solve(TreeNode* root, int targetSum){
         if(!root)
             return;
-        ans.push_back(root->val);
-        solve(root->left,targetSum);
+        ans.push_back(root->val); // push value of every node
+        solve(root->left,targetSum); 
          solve(root->right,targetSum);
-        if(root->left==NULL && root->right==NULL)
+        if(root->left==NULL && root->right==NULL) // if we have come to aleaf node, we need to find sum now
         {
             int s=0;
             for(int i=0;i<ans.size();i++)
                 s+=ans[i];
             if(s==targetSum)
+            {
                 check=true;
+                return;
+            }
         }
-        ans.pop_back();
+        ans.pop_back();// now pop out the last element
     }
     bool hasPathSum(TreeNode* root, int targetSum) {
         solve(root, targetSum);
