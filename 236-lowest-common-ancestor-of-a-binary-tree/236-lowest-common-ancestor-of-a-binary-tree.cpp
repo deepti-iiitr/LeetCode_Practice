a1b5a092a4e3c003f37f3332b57bf2e677810abd
Deepti <64003317+deepti-iiitr@users.noreply.github.com>
2022-08-31 01:46:49 +0530

    Time: 19 ms (79.52%), Space: 17.4 MB (6.05%) - LeetHub

---
+++ 236-lowest-common-ancestor-of-a-binary-tree/236-lowest-common-ancestor-of-a-binary-tree.cpp
@@ -0,0 +1,51 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    bool path(TreeNode* root,vector<TreeNode*> &v,TreeNode* &t)
+    {
+        if(root==NULL)
+            return false;
+        v.push_back(root);
+        if(root==t)
+        {
+            //v.push_back(root);
+            return true;
+        }
+        if(path(root->left,v,t))
+            return true;
+        if(path(root->right,v,t))
+            return true;
+        v.pop_back();
+        return false;
+    }
+    TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
+        vector<TreeNode*> vp;
+        vector<TreeNode*> vq;
+        path(root,vp,p);
+        path(root,vq,q);
+        int psize=vp.size();
+        int qsize=vq.size();
+        int min;
+        if(psize<qsize)
+            min=psize;
+        else
+            min=qsize;
+        TreeNode* lca;
+        for(int i=0;i<min;i++)
+        {
+            if(vp[i]==vq[i])
+                lca=vp[i];
+            else
+                break;
+        }
+        return lca;
+    }
+};
